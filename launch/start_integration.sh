faa15c1a5b4fd606b82f2565a917eaebe792dce6
tongbochen <tongbochen@zhidaoauto.com>
2020-12-04 14:37:15 +0800

    modify start_intergration script

--- launch/start_integration.sh
+++ launch/start_integration.sh
@@ -5,7 +5,7 @@ ps aux | grep roscore |awk '{print $2}' |xargs kill
 sudo chmod +777 /dev -R
 
 echo $HOME
-source $HOME/autopilot/install/setup.sh
+source $HOME/autopilot/setup.sh
 
 ###########################################################
 GLOG_COMMAND="export GLOG_logtostderr=1; export GLOG_colorlogtostderr=1"
