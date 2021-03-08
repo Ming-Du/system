a9334983bbb8a3de4a684b85ec3a499844c9cce1
zhaoyunhui <zhaoyunhui@zhidaoauto.com>
2021-03-08 20:02:00 +0800

    cd

--- launch/autopilot.sh
+++ launch/autopilot.sh
@@ -2,4 +2,6 @@
 sed -i 's/\(${_CATKIN_SETUP_DIR:=\).*/\1\/home\/mogo\/autopilot}/g' $HOME/autopilot/setup.sh
 source $HOME/autopilot/setup.sh
 
+cd $HOME/autopilot/  
+
 source $HOME/autopilot/share/launch/integration.sh
