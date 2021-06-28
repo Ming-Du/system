ca4244bbcd6efd399acee81a85ddad87c07c37de
sunhaihui <sunhaihui@mogoauto.com>
2021-06-28 08:05:16 +0000

    使用/home/mogo代替$HOME

--- launch/autopilot_xfce_silence_debug.sh
+++ launch/autopilot_xfce_silence_debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $HOME
 
-source ~/catkin_ws/devel/setup.bash
+source /home/mogo/catkin_ws/devel/setup.bash
 BASHRC="/home/mogo/catkin_ws/src/system/launch/bashrc_debug.sh"
-source ~/catkin_ws/src/system/launch/integration_xfce_silence.sh
+source /home/mogo/catkin_ws/src/system/launch/integration_xfce_silence.sh
