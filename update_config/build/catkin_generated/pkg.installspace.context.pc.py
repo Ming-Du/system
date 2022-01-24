d2604eb926651eae1648eefa109defcbfae13802
root <root>
2022-01-24 05:23:59 +0000

    delete build tags devel

--- update_config/build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ldrivers_camera".split(';') if "-ldrivers_camera" != "" else []
-PROJECT_NAME = "drivers_camera"
-PROJECT_SPACE_DIR = "/home/mogo/data/catkin_ws/src/drivers/install"
-PROJECT_VERSION = "0.3.6"
