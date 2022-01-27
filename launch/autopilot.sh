2df1f63240f9de9d7b982cdbfb74fed0ea05c724
jiahongfei <jiahongfei@mogoauto.com>
2022-01-27 14:25:52 +0800

    other[ros]:fix search path

--- launch/autopilot.sh
+++ launch/autopilot.sh
@@ -140,9 +140,9 @@ Logging() {
 }
 install_ros_log()
 {
-     src_so_path=`find  /home/mogo/ -name 'libroscpp.so' | head -n 1`
+     src_so_path=`find  /autocar-code/install/  -name 'libroscpp.so' | head -n 1`
      dst_so_path=`find  /opt -name  'libroscpp.so' | head -n 1`
-     conf_path=`find  /home/mogo/ -name 'ros_statics.conf' | head -n 1`
+     conf_path=`find   /autocar-code/install/   -name 'ros_statics.conf' | head -n 1`
      dst_conf_path="/home/mogo/data/log/ROS_STAT/ros_statics.conf"
      mkdir -p /home/mogo/data/log/ROS_STAT/
      chmod 777 -R /home/mogo/data/log/
