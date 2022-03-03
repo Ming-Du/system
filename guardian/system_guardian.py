1052dd3465276a7793e0cabb78e2e2bc387303f1
jiahongfei <jiahongfei@mogoauto.com>
2022-03-03 16:02:47 +0800

    other[system]:add log output sub process rosnode ping -a

--- guardian/system_guardian.py
+++ guardian/system_guardian.py
@@ -287,11 +287,14 @@ class Process(Thread):
   def node_ping_alive(self):
       ping_flag = False
       node_alive_li = []
+      print "bebore rosnode ping -a ========="
       prog = Popen("rosnode ping -a", shell=True, stdout=PIPE)
+      print "after rosnode ping -a ========"
       node_li = prog.stdout.read()
       #output = prog.communicate()
       #output_li = list(output)
       node_li = node_li.split("\n")
+      print "node_ping_alive for before====="
       for ping_line in node_li:
           ping_li = ping_line.split(" ")
           if ping_li[0] == "pinging":
@@ -301,6 +304,7 @@ class Process(Thread):
              node_alive_li.append(ping_li[1])
           else:
              ping_flag = False
+        print "node_ping_alive for after======"
       print(node_alive_li)
       return node_alive_li        
       
