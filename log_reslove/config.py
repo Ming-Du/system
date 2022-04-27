cf93f8cd3f292cbd8b50ceac22b5a2db681b762f
liyuelei <liyuelei@zhidaoauto.com>
2022-04-27 14:10:37 +0800

    mod topic name form camera_obstacle to camera_obstacle_front60 because 2D_front changed

--- log_reslove/config.py
+++ log_reslove/config.py
@@ -47,7 +47,7 @@ node_config["/sensor/lidar/c32/front_right/c32_right_decoder"]["pub"] = "/sensor
 
 node_config["/perception_camera_2D_front"] = {}
 node_config["/perception_camera_2D_front"]["sub"] = ["/sensor/camera/sensing/image_raw_60/nvjpeg"]
-node_config["/perception_camera_2D_front"]["pub"] = "/perception/camera/camera_obstacle"
+node_config["/perception_camera_2D_front"]["pub"] = "/perception/camera/camera_obstacle_front60"
 
 node_config["/sensor/camera/sensing60/drivers_camera_sensing60"] = {}
 node_config["/sensor/camera/sensing60/drivers_camera_sensing60"]["sub"] = []
