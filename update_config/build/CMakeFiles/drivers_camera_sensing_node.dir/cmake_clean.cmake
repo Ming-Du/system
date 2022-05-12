6ad40d261269ba978c7c3b34799e3c231a34d1e2
qixianghui <qixianghui@zhidaoauto.com>
2022-05-12 07:10:58 +0000

    add[system] add update_config

---
+++ update_config/build/CMakeFiles/drivers_camera_sensing_node.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/mogo/data/catkin_ws/src/drivers/devel/lib/drivers_camera/drivers_camera_sensing_node"
+  "/home/mogo/data/catkin_ws/src/drivers/devel/lib/drivers_camera/drivers_camera_sensing_node.pdb"
+  "CMakeFiles/drivers_camera_sensing_node.dir/sensing/sensing_camera_node.cpp.o"
+  "CMakeFiles/drivers_camera_sensing_node.dir/sensing/sensing_camera_node.cpp.o.d"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/md5.cpp.o"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/md5.cpp.o.d"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/mogo_curl.cpp.o"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/mogo_curl.cpp.o.d"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/mogo_json.cpp.o"
+  "CMakeFiles/drivers_camera_sensing_node.dir/src/mogo_json.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drivers_camera_sensing_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ update_config/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mogo/data/catkin_ws/src/drivers/camera
+# Build directory: /home/mogo/data/catkin_ws/src/drivers/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
