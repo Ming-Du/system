ef4bada57d8dbee28f229fe07ae0205e2c93b54b
liyuelei <liyuelei@zhidaoauto.com>
2022-07-13 22:02:50 +0800

    mod[log_reslove] update log_reslove for MAP260

--- log_collect_client/log_collect_client.py
+++ log_collect_client/log_collect_client.py
@@ -3,11 +3,12 @@
 import os
 import socket
 import time
+import datetime
 
 work_dir = "/home/mogo/data/log"
-#work_dir = "/tmp/keylog_parser"
 src_dir = os.path.join(work_dir, "ROS_STAT", "EXPORT")
 tmp_dir = os.path.join(work_dir, "ROS_STAT_TMP")
+bak_dir = os.path.join(work_dir, "ROS_STAT", datetime.date.today())
 
 # scp -l Kb/s
 def run_once():
@@ -19,13 +20,16 @@ def run_once():
         try:
             sock.connect(('rosslave-103', 1119))
         except Exception as e:
-            return 
+            print('sock connect error, {}'.format(e))
+            
     # 先截取一遍文件
     files = os.listdir(src_dir)
     for file_name in files:
+        if '.tmp' in file_name:
+            # used .tmp match file don't handel
+            continue
         file_path = os.path.join(src_dir, file_name)
         tmp_file_path = os.path.join(tmp_dir, file_name)
-
         os.rename(file_path, tmp_file_path)
 
     # 将截取数据发出去
@@ -34,17 +38,13 @@ def run_once():
         tmp_file_path = os.path.join(tmp_dir, file_name)
         with open(tmp_file_path, "rb") as fp:
             sock.sendfile(fp)
+        bak_file_path = os.path.join(bak_dir, file_name)
+        os.rename(tmp_file_path, bak_file_path)
 
     sock.close()
-
-    os.system("rm -f {0}/*".format(tmp_dir))
+    #os.system("rm -f {0}/*".format(tmp_dir))
 
 def run():
-    if os.path.exists(tmp_dir) == False:
-        os.mkdir(tmp_dir)
-    if os.path.exists(src_dir) == False:
-        os.makedirs(src_dir) 
-
     while True:
         start = time.time()
         run_once()
@@ -54,6 +54,13 @@ def run():
             time.sleep(1.0 - (end - start))
 
 def main():
+    if os.path.exists(tmp_dir) == False:
+        os.mkdir(tmp_dir)
+    if os.path.exists(src_dir) == False:
+        os.makedirs(src_dir) 
+    if os.path.exists(bak_dir) == False:
+        os.mkdir(bak_dir)
+
     run()
 
 if __name__ == '__main__':
